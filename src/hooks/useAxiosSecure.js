a1c771cb09bbbbcf3c3b7d5f286708ab201f4afc
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-10-22 14:36:12 +0600

    add useAuth and useAxiosSecure

---
+++ src/hooks/useAxiosSecure.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import useAuth from './useAuth';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export const axiosSecure = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+})
+
+
+const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(
+            res => {
+                return res
+            }, async error => {
+                console.log('error caught from our very own axios interceptor--->', error.response)
+                if (error.response.status === 401 || error.response.status === 403) {
+                    // logout
+                    logOut();
+                    // navigate to login
+                    navigate('/login')
+                }
+            }
+
+        )
+    }, [logOut, navigate])
+    return axiosSecure
+};
+
+export default useAxiosSecure;
